13bb7b31bfb46de1e3f526ff2838cb24b2b0e64d
Javier Moreno <mq2880@gmail.com>
2021-01-14 10:19:40 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Module 2: Querying and Manipulating Data Using Entity Framework
+
+# Lesson 3: Querying Data
+
+### Demonstration: Using Language-Integrated Query (LINQ) to Entities
+
+- Abrir el proyecto de consola UsingLINQtoEntities en Visual Studio Code 
+- Creamos el contexto en el main e inicializamos la BD
+- Ejecutar
+
+
+
+![](./img/Captura1.jpg)
+
---
+++ Starter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using UsingLINQtoEntity.Database;
+using UsingLINQtoEntity.Models;
+
+namespace UsingLINQtoEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                DbInitializer.Initialize(context);
+                var courses = from c in context.Courses
+                              select c;
+                foreach (var course in courses)
+                {
+                    Console.WriteLine($"Course: {course.Name}");
+                    foreach (var student in course.Students)
+                    {
+                        Console.WriteLine($"\t Student name: {student.Name}");
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
